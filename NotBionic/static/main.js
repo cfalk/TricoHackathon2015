f994541eaeb7272c4cb5a6a267dd8bd4e2657b92
Jason Feinberg <github@redlxx.com>
2015-01-23 23:22:47 -0500

    Added the bindFilterOpen function
    Added the function that is used to open the specific filter options div
    on the explore page. It currently will run on ALL pages, but it won't do
    anything on the non-explore pages.

--- NotBionic/static/main.js
+++ NotBionic/static/main.js
@@ -1,2 +1,26 @@
 $( document ).ready(function() {
+
+	//Click binds
+	function bindFilterOpen(){
+		$(".filter").on("click", function(){
+			var $this = $(this); //Used to speed to up calls to this $(this)
+			var dataDiv = $this.data("filter");
+			var $correspondingFilterOptionsDiv = $("#"+dataDiv+"-filterOptions"); //Similarly use to speed up calls
+			if ($correspondingFilterOptionsDiv.hasClass("activeFilterOptions")){
+				$correspondingFilterOptionsDiv.toggleClass("activeFilterOptions");
+				$correspondingFilterOptionsDiv.toggleClass("disabledFilterOptions");
+			}
+			else{
+				var $currentActive = $("activeFilterOptions");
+				if ($currentActive){
+					$currentActive.toggleClass("activeFilterOptions");
+					$currentActive.toggleClass("disabledFilterOptions");
+				}
+				$correspondingFilterOptionsDiv.toggleClass("activeFilterOptions");
+				$correspondingFilterOptionsDiv.toggleClass("disabledFilterOptions");
+			}
+		});
+	}
+
+	bindFilterOpen();
 });
