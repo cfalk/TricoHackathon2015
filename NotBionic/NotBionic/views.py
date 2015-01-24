bc1498a48d329a2225545e987d730dab4e4ae915
Brandon Smith <bsmith8108@gmail.com>
2015-01-23 19:53:56 -0500

    forgot to commit the view.py file

---
+++ NotBionic/NotBionic/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def explore(request):
+	return HttpResponse("Hello world! This is the explore page")
+
+def profile(request):
+	return HttpResponse("This is the profile page")
