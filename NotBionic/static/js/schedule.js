474ef2419583c1f788c4af53a551eba917110211
Brandon Smith <bsmith8108@gmail.com>
2015-01-24 10:52:10 -0500

    forgot to add schedule.js

---
+++ NotBionic/static/js/schedule.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var schedule = $('#schedule');
+    // Create the header information for the table
+    schedule.append('<table id=schedule>');
+    schedule.append('<tr><th>Times</th><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr>');
+    hello
+});
