5f4cb20df1649a09178d036354494a5267be54c9
Blair Rush <btrush@blair-linux.(none)>
2015-01-23 21:43:31 -0500

    main.js created and some changes to explore.html

--- NotBionic/NotBionic/views.py
+++ NotBionic/NotBionic/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def explore(request):
-	return HttpResponse("Hello world! This is the explore page")
+	return render(request, "explore.html")
 
 def profile(request):
 	return render(request,"profile.html")
