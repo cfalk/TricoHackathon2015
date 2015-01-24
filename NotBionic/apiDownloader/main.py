0a780a812c3c38f0cd13a12bed04da1646124136
Casey Falk <caseyfalk94@gmail.com>
2015-01-23 22:09:35 -0800

    Committing progress on cleaning API info.

--- NotBionic/apiDownloader/main.py
+++ NotBionic/apiDownloader/main.py
@@ -2,11 +2,9 @@ def getCourse(college, semester, reg):
   import subprocess
   import json
 
-  url = "http://dev-trico-cc.haverford.edu/api/course/get"
   command = "curl -i -H \"Content-Type: application/json\" -X POST -d '{{\"college\":\"{}\", \"semester\":\"{}\", \"reg_id\":\"{}\"}}' http://dev-trico-cc.haverford.edu/api/course/get".format(college, semester, reg)
 
   raw = subprocess.check_output(command, shell=True)
-  print "____________________"
 
   # Remove the "header" information.
   response = "".join(raw.split("\n")[6:])
@@ -15,14 +13,53 @@ def getCourse(college, semester, reg):
   return json.loads(response)["response"][0]
 
 
+def parse_info(raw_info):
+  import re
+
+  info = {}
+
+  # Grab the basic data.
+  headers_to_rip = [
+                    "number", "title", "dept_name", "instructor", "semester",
+                    "days","end_times","start_times", "reg_id", "location",
+                    "college"
+                    ]
+
+  for header in headers_to_rip:
+    info[header] = raw_info[header]
+    del raw_info[header]
+
+  misc_info = raw_info["misc"]
+
+  # Strip any "key: value" text from the `misc_info`.
+  regex = re.compile("([a-zA-z ]+: [a-zA-Z0-9]+)")
+  for match in regex.finditer(misc_info[0]):
+    key, val = match.group().split(":")
+    info[key.strip()] = val.strip()
+
+  # Get the course's division from `misc_info` if it's available.
+  if ";" in misc_info[0]:
+    info["division"] = misc_info[0].split(";")[1].strip()
+  else:
+    info["division"] = ""
+
+  # Get the course's description
+  print misc_info[1].split("|")
+
+
+  #print raw_info
+  #print info
+  return info
+
+
 def main():
   courses = range(10)
   errors = 0
 
   for course in courses:
     try:
-      info = getCourse("haverford","spring_2014","PSYCH100A001")
-      print info
+      info = getCourse("haverford","fall_2014","ANTHH103A001")
+      info = parse_info(info)
 
     except Exception as e:
       print e
