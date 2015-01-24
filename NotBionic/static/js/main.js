6c134ba481a59a292c20641f27ae703cedf2c9cb
Gabe <grybeck@gmail.com>
2015-01-24 12:02:51 -0500

    Added fancybox

--- NotBionic/static/js/main.js
+++ NotBionic/static/js/main.js
@@ -1,4 +1,17 @@
 $( document ).ready(function() {
+	
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
 	//Click binds
 	function bindFilterOpen(){
 		$(".filter").on("click", function(){
