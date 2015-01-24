f54610f5151d8fa60805c851f6efda903ff61995
Brandon Smith <bsmith8108@gmail.com>
2015-01-23 23:16:13 -0500

    some changes, Blair needed the settings file

--- NotBionic/NotBionic/views.py
+++ NotBionic/NotBionic/views.py
@@ -5,4 +5,26 @@ def explore(request):
 	return render(request, "explore.html")
 
 def profile(request):
-	return render(request,"profile.html",{'classes':{'eight':['on','off','on','off','on'],'nine':['on','off','on','off','on'],'ten':['on','off','on','off','on'],'eleven':['on','off','on','off','on'],'twelve':['on','off','on','off','on']},'user':{'name':'Brandon'}})
+	return render(request,"profile.html",{'classes':{
+	'eight':['off','on','on','off','on','off','on'],
+	'eight_2':['off','on','on','off','on','off','on'],
+	'nine':['off','on','on','off','on','off','on'],
+	'nine_2':['off','on','on','off','on','off','on'],
+	'ten':['off','on','on','off','on','off','on'],
+	'ten_2':['off','on','on','off','on','off','on'],
+	'eleven':['off','on','on','off','on','off','on'],
+	'eleven_2':['off','on','on','off','on','off','on'],
+	'twelve':['off','on','on','off','on','off','on'],
+	'twelve_2':['off','on','on','off','on','off','on'],
+	'one':['off','on','on','off','on','off','on'],
+	'one_2':['off','on','on','off','on','off','on'],
+	'two':['off','on','on','off','on','off','on'],
+	'two_2':['off','on','on','off','on','off','on'],
+	'three':['off','on','on','off','on','off','on'],
+	'three_2':['off','on','on','off','on','off','on'],
+	'four':['off','on','on','off','on','off','on'],
+	'four_2':['off','on','on','off','on','off','on'],
+	},
+	'user':{'name':'Brandon'}})
+
+
