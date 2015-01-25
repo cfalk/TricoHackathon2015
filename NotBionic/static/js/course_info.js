e0982e883d571220ea534c51674d6d0865fbc9ec
Gabe <grybeck@gmail.com>
2015-01-25 11:48:39 -0500

    stuff on course_info.html

--- NotBionic/static/js/course_info.js
+++ NotBionic/static/js/course_info.js
@@ -1,10 +1,9 @@
-$(document).on("ready", function() {
 
-  alert("HHERE")
-
+  console.log("hello");
   // Load the icon.
   var dept = $(".big_card_course_id").substr(0,4);
   var iconSrc = iconDict[dept];
   $(".big_card_icon img").attr("src",iconSrc);
 
-})
+  $(".big_card_time").text() = "Ahh";
+
