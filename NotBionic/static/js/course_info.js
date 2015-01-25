95c8809a6833d1cf05bde9c0d2b24ce6e96b1434
Gabe <grybeck@gmail.com>
2015-01-25 13:13:43 -0500

    adding styling to big cards

--- NotBionic/static/js/course_info.js
+++ NotBionic/static/js/course_info.js
@@ -1,9 +1,26 @@
 
-  console.log("hello");
+function prepareCardImage() {
   // Load the icon.
-  var dept = $(".big_card_course_id").substr(0,4);
+  var dept = $(".big_card_course_id").html().trim().substring(0,4);
   var iconSrc = iconDict[dept];
-  $(".big_card_icon img").attr("src",iconSrc);
+  $(".big_card_icon img").attr("src", "/static/images/"+iconSrc);
+ }
 
-  $(".big_card_time").text() = "Ahh";
 
+function arrowButton() {
+  return "<div class='button arrow-button'>" +
+            "<img src='/static/images/arrow.jpg'>" +
+          "</div>";
+}
+
+function hide() {
+	$(".more_info").hide()
+}
+
+function show() {
+	$(".more_info").show()
+}
+
+$(".big-card-arrow-button").on("click", function(){
+	show();
+});
